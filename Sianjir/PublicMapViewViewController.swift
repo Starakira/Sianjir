46297879a22d29d900ff2139fdab3d3d294c2cb7
William Sebastian <chenyusen1999@gmail.com>
2020-03-12 22:06:59 +0700

    Ini HAsil yg nerusin didesign + sama buat tombol krl + mrt nya bisa brubah warna kalau di klik tolong bantu connect in

--- Sianjir/PublicMapViewViewController.swift
+++ Sianjir/PublicMapViewViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 class PublicMapViewViewController: UIViewController {
 
     @IBOutlet weak var buttonKRL: UIView!
-    
+    var buttongantiwarna = "no"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
      
     }
+    @IBAction  func buttonkrlfunc (sender: Any){
+        buttonkrlvar.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    }
+    @IBAction  func buttonmrtfunc (sender: Any){
+        buttonmrtvar.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    }
+    @IBOutlet weak var buttonkrlvar: UIView!
+    @IBOutlet weak var buttonmrtvar: UIView!
     
+    
+
 
 }
