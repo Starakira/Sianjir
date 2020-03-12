649ed8145b9593f739ac556013fcc9dc4925f1ce
Maulana Ardiansyah <maul.ardiansyahh@gmail.com>
2020-03-12 13:57:12 +0700

    baru terbaru

--- Sianjir/InfoViewController.swift
+++ Sianjir/InfoViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
-     @IBOutlet weak var firstView: UIView!
-       @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
 
        override func viewDidLoad() {
            super.viewDidLoad()
-           // Do any additional setup after loading the view.
+            
        }
 
        @IBAction func switchViews(sender: UISegmentedControl){
