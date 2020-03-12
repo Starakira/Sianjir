417b8cc5bd0632ec8896196ae24c9b39526a0f3e
Ryan Kohari <ryankhohari@gmail.com>
2020-03-12 17:27:57 +0700

    Tambahan Buat william

---
+++ Sianjir/PublicMapViewViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PublicMapViewViewController.swift
+//  Sianjir
+//
+//  Created by Ryan Anslyno Khohari on 12/03/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class PublicMapViewViewController: UIViewController {
+
+    @IBOutlet weak var buttonKRL: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+}
