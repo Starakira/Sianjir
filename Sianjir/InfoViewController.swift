fd92ef0b906ba7c8929ffe52392884267cbe9159
Ryan Kohari <ryankhohari@gmail.com>
2020-03-25 20:58:14 +0700

    modal view

--- Sianjir/InfoViewController.swift
+++ Sianjir/InfoViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import BonsaiController
 
 class InfoViewController: UIViewController {
 
@@ -17,8 +18,24 @@ class InfoViewController: UIViewController {
            super.viewDidLoad()
             
        }
+    
+    
+    @IBAction func UIModal(_ sender: Any) {
+        let smallVC = storyboard!.instantiateViewController(identifier: "SmallVC")
+        smallVC.transitioningDelegate = self
+        smallVC.modalPresentationStyle = .custom
+        present(smallVC, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
-       @IBAction func switchViews(sender: UISegmentedControl){
+        if segue.destination is InfoViewController {
+            segue.destination.transitioningDelegate = self
+            segue.destination.modalPresentationStyle = .custom
+        }
+    }
+    
+    @IBAction func switchViews(sender: UISegmentedControl){
         switch sender.selectedSegmentIndex {
         case 1:
             mapView.mapType = .hybrid
@@ -29,3 +46,21 @@ class InfoViewController: UIViewController {
     }
 }
 
+extension InfoViewController: BonsaiControllerDelegate {
+    
+    // return the frame of your Bonsai View Controller
+    func frameOfPresentedView(in containerViewFrame: CGRect) -> CGRect {
+        
+        return CGRect(origin: CGPoint(x: 0, y: containerViewFrame.height / 2), size: CGSize(width: containerViewFrame.width, height: containerViewFrame.height / 2))
+    }
+    
+    // return a Bonsai Controller with SlideIn or Bubble transition animator
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+    
+        // Slide animation from .left, .right, .top, .bottom
+        return BonsaiController(fromDirection: .bottom, blurEffectStyle: .light, presentedViewController: presented, delegate: self)
+        
+        // or Bubble animation initiated from a view
+        //return BonsaiController(fromView: yourOriginView, blurEffectStyle: .dark,  presentedViewController: presented, delegate: self)
+    }
+}
---
+++ Sianjir/MapModalView.swift
@@ -0,0 +1,12 @@
+//
+//  MapModalView.swift
+//  Sianjir
+//
+//  Created by Ryan Anslyno Khohari on 25/03/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
