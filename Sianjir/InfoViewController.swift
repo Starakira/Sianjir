d0674c8ea74549dcee95f01f0ac81c4f9d91547c
Muhammad Bangun Agung <bangunagung1@gmail.com>
2020-03-25 21:40:54 +0700

    New version with user location and indicator.

--- Sianjir/InfoViewController.swift
+++ Sianjir/InfoViewController.swift
@@ -11,21 +11,138 @@ import MapKit
 
 class InfoViewController: UIViewController {
 
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate: CLLocationCoordinate2D?
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
-           super.viewDidLoad()
-            
+        super.viewDidLoad()
+        mapView.delegate = self
+        configureLocationServices()
        }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    private func configureLocationServices() {
+        locationManager.delegate = self
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined{            locationManager.requestAlwaysAuthorization()
+        } else if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdates(locationManager: locationManager)
+        }
+    }
+    
+    private func beginLocationUpdates(locationManager: CLLocationManager) {
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion((zoomRegion), animated: true)
+    }
+    
+    private func addFloodAnnotations() {
+        let lightFloodSymbol = MKPointAnnotation()
+        lightFloodSymbol.title = "Light Flood"
+        lightFloodSymbol.coordinate = CLLocationCoordinate2D(latitude: -6.273292, longitude: 106.741036)
+        
+        let mediumFloodSymbol = MKPointAnnotation()
+        mediumFloodSymbol.title = "Medium Flood"
+        mediumFloodSymbol.coordinate = CLLocationCoordinate2D(latitude: -6.073292, longitude: 106.841036)
+        
+        let heavyFloodSymbol = MKPointAnnotation()
+        heavyFloodSymbol.title = "Heavy Flood"
+        heavyFloodSymbol.coordinate = CLLocationCoordinate2D(latitude: -6.3721, longitude: 106.941036)
+        
+        mapView.addAnnotation(lightFloodSymbol)
+        mapView.addAnnotation(mediumFloodSymbol)
+        mapView.addAnnotation(heavyFloodSymbol)
+    }
+    
+    private func resizeAnnotationImage(imageName: String, imageWidth: Double, imageHeight:Double) -> UIImage{
+        let image = UIImage(named: imageName)
+        let resizedSize = CGSize(width: imageWidth, height: imageHeight)
+        
+        UIGraphicsBeginImageContext(resizedSize)
+        image?.draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage!
+    }
 
-       @IBAction func switchViews(sender: UISegmentedControl){
+   @IBAction func switchViews(sender: UISegmentedControl){
         switch sender.selectedSegmentIndex {
         case 1:
             mapView.mapType = .hybrid
         default:
             mapView.mapType = .standard
         }
-           
+    }
+    
+    @IBAction func showUserLocation(_ sender: Any) {
+        let userLocation = locationManager.location
+        guard let userCoordinate = userLocation?.coordinate else { return }
+        
+        zoomToLatestLocation(with: userCoordinate)
     }
 }
 
+extension InfoViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did get latest location")
+        
+        guard let latestLocation = locations.first else { return }
+        
+        if currentCoordinate == nil {
+            zoomToLatestLocation(with: latestLocation.coordinate)
+            addFloodAnnotations()
+        }
+        
+        currentCoordinate = latestLocation.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("The status changed")
+        
+        if status == .authorizedAlways || status == .authorizedWhenInUse{
+            beginLocationUpdates(locationManager: manager)
+        }
+    }
+}
+
+extension InfoViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
+
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
+        }
+        
+        if let title = annotation.title, title == "Light Flood" {
+            annotationView?.image = resizeAnnotationImage(imageName: "Banjir Ringan", imageWidth: 30, imageHeight: 30)
+        } else if let title = annotation.title, title == "Medium Flood" {
+            annotationView?.image = resizeAnnotationImage(imageName: "Banjir Sedang", imageWidth: 30, imageHeight: 30)
+        } else if let title = annotation.title, title == "Heavy Flood" {
+            annotationView?.image = resizeAnnotationImage(imageName: "Banjir Parah", imageWidth: 30, imageHeight: 30)
+        } else if annotation === mapView.userLocation {
+            annotationView?.image = UIImage(systemName: "circle.fill")
+        }
+        
+        annotationView?.canShowCallout = true
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("The annotation was selected : \(String(describing: view.annotation?.subtitle))")
+    }
+}
